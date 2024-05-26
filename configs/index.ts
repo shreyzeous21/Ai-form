b94eb06f3f416fe284f4d0d484e2ade2e38e7458
Shrey_sadhukhan <shrey.sadhukhan21@gmail.com>
2024-05-27 02:18:31 +0530

    Add files via upload

---
+++ configs/index.ts
@@ -0,0 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL);
+export const db = drizzle(sql, { schema });
